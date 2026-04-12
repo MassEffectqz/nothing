cdcb9400b6fb0a3a2c94215524882181f3e547ad
MassEffectqz <hatikohakki@gmail.com>
2026-04-13 01:14:33 +0300

    chore: force rebuild with new JS bundle hash to fix Netlify CDN delivery issue

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,10 @@ import { BrowserRouter } from 'react-router-dom'
 import { LanguageProvider } from './hooks/useLanguage.jsx'
 import App from './App.jsx'
 
+// Build: 2026-04-13
+const BUILD_ID = 'v2-rebuild'
+console.info('App initialized:', BUILD_ID)
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <LanguageProvider>
